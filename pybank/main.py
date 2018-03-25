a9c61b97cb23815778d90f6cc024e6d91ff0ee5f
SpeshK <kc.speshk@gmail.com>
2018-03-24 20:44:04 -0500

    New Homework

--- pybank/main.py
+++ pybank/main.py
@@ -5,33 +5,56 @@ import numpy as np
 
 df = pd.read_csv('budget_data_1.csv', parse_dates=['Date'])
 
-print(df[:3])
-print(df['Date'])
-
-print(list(df))
-print(df.iloc[1],[1])
-
 dateparse = lambda x: pd.datetime.strptime(x, '%b-%y')
 
 df = pd.read_csv('budget_data_1.csv', parse_dates=['Date'], date_parser=dateparse)
 
-print(df)
+nummonths = pd.to_numeric(df.drop_duplicates('Date').count()).iloc[0]
 
-input1 = input("Continue")
+f= open("budget.txt","w")
 
-nummonths = pd.to_numeric(df.drop_duplicates('Date').count())
+print("\n Financial Analysis \n ",\
+    "------------------- \n")
+f.write("Financial Analysis \n ------------------- \n")
 
-print(nummonths)
+print("Total Number of Months:     " + str(nummonths))
+f.write("Total Number of Months:     ")
+f.write(str(nummonths))
 
 df = df.sort_values('Date')
 rev1 = df.iloc[0,1]
-print(rev1)
+print("\n Initial Revenue: $" + str(rev1))
+f.write("\n Initial Revenue: $")
+f.write(str(rev1))
+
 df = df.sort_values('Date', ascending=False)
 rev2 = df.iloc[0,1]
-print(rev2)
+
+print("\n Ending Revenue: $" + str(rev2))
+f.write("\n Ending Revenue: $")
+f.write(str(rev2))
+
 totalrev = rev2 - rev1
-print(totalrev)
+print("\n Total Revenue: $" + str(totalrev))
+f.write("\n Total Revenue: $")
+f.write(str(totalrev))
 
 averagerev = totalrev / nummonths
 
-print(averagerev)
+print("\n Average Revenue Per Month: $" + str(round(averagerev,2)))
+f.write("\n Average Revenue Per Month: $")
+f.write(str(round(averagerev,2)))
+
+series1 = df.Revenue.max()
+
+print("\n Greatest Increase in Revenue: $" + str(series1))
+f.write("\n Greatest Increase in Revenue: $")
+f.write(str(series1))
+
+series2 = df.Revenue.min()
+print("\n Greatest Decrease in Revenue: $" + str(series2))
+f.write("\n Greatest Decrease in Revenue: $")
+f.write(str(series2))
+
+print("\n Have A Nice Day XOXO")
+f.close()
